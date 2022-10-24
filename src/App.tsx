eece6af5c8ccb793cf7c5af18512ba9c0a11f49a
Jacob Williams <supergeekjake@users.noreply.github.com>
2022-10-24 00:30:15 -0700

    feat: add feature filtering logic
    Closes #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { type Component, For, Show, createSignal } from "solid-js";
+import { type Component, For, Show, createSignal, createMemo } from "solid-js";
 
-import { locations } from "./locations";
+import { visits, locationMap } from "./locations";
+import Visit from "./Visit";
 
 const App: Component = () => {
   const [showFeatured, setShowFeatured] = createSignal(true);
   const toggle = () => setShowFeatured(!showFeatured());
+  const list = createMemo(() => {
+    if (!showFeatured()) return visits;
+    return visits.filter((v) => {
+      const loc = locationMap[v.locationId];
+      return loc.tags.includes("featured");
+    });
+  });
 
   return (
     <div class="grid">
-      {/* TODO: toggle to show all visits vs featured.
-          TODO+: They are separate buttons due to different aria-labels. The only changing condition visually is the color of the small circle in the button. Would be nice if this was refactored o_O 
-      */}
       <Show
         when={showFeatured()}
         fallback={
@@ -33,22 +38,9 @@ const App: Component = () => {
         </button>
       </Show>
       <main class="grid grid-cols-4 gap-0.5 border-2 border-neutral-900 bg-neutral-900">
-        <For each={locations}>
+        <For each={list()}>
           {(item) => (
-            <div class="bg-white">
-              <img
-                src="https://via.placeholder.com/150"
-                class="w-full bg-cover"
-              />
-              <div class="px-4 py-3 border-t border-neutral-900">
-                <h3 class="font-bold text-lg leading-6">
-                  <span>{item.name}</span>
-                  <Show when={item.tags.includes("featured")}>
-                    <span>⭐</span>
-                  </Show>
-                </h3>
-              </div>
-            </div>
+            <Visit visit={item} location={locationMap[item.locationId]} />
           )}
         </For>
       </main>
---
+++ src/Visit.tsx
@@ -0,0 +1,21 @@
+import { type Component, Show } from "solid-js";
+
+import { type IVisit, type ILocation } from "./locations";
+
+const Visit: Component<{ visit: IVisit; location: ILocation }> = (props) => {
+  return (
+    <div class="bg-white">
+      <img src="https://via.placeholder.com/150" class="w-full bg-cover" />
+      <div class="px-4 py-3 border-t border-neutral-900">
+        <h3 class="font-bold text-lg leading-6">
+          <span>{props.location.name}</span>
+          <Show when={props.location.tags.includes("featured")}>
+            <span>⭐</span>
+          </Show>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Visit;
