dfc2afaf9ebe8629e3280582512126b4da8dd61c
oliviale <oliviale@buffalo.edu>
2022-10-27 13:03:18 +0800

    feat: new UI

--- src/Visit.tsx
+++ src/Visit.tsx
@@ -3,14 +3,17 @@ import { formatDistance } from "date-fns";
 
 const Visit: Component<{ visit: IVisit; location: ILocation }> = (props) => {
   return (
-    <div class="bg-white">
-      <img src="https://via.placeholder.com/150" class="w-full bg-cover" />
-      <div class="px-4 py-3 border-t border-neutral-900">
-        <h3 class="font-hackyd text-2xl leading-6">
+    <div class="bg-amber-100 border-2 border-neutral-900  p-4 pb-2 md:p-5 md:pb-3 shadow-solid">
+      <div class="flex gap-2 relative">
+        <img src="https://via.placeholder.com/150" class="w-full bg-cover flex-1 rounded-2xl" />
+        <p class="[writing-mode:vertical-rl] text-neutral-600 italic -mr-2">Visit description</p>
+        <Show when={props.location.tags.includes("featured")}>
+          <span class="absolute -bottom-0.5 -right-0.5 text-lg float-right">⭐</span>
+        </Show>
+      </div>     
+      <div class="py-2 border-t mt-3 border-neutral-900">
+        <h3 class="font-serif text-xl mt-1 leading-none 2xl:text-3xl md:leading-tight">
           <span>{props.location.name}</span>
-          <Show when={props.location.tags.includes("featured")}>
-            <span>⭐</span>
-          </Show>
         </h3>
         <div class="text-sm text-neutral-900">
           {formatDistance(props.visit.date, new Date())}
