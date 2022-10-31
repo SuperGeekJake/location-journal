cd83f37576b907e831a952d48b11450213df7271
Jacob Williams <supergeekjake@users.noreply.github.com>
2022-10-31 11:14:48 -0700

    feat: Build function to pull data from Google Sheets

--- functions/visits.ts
+++ functions/visits.ts
@@ -1,12 +1,71 @@
 import { Handler } from "@netlify/functions";
+import * as google from "@googleapis/sheets";
 
-import { visits } from "../src/database";
+export const handler: Handler = async (event, context) => {
+  try {
+    if (!process.env.GOOGLE_CREDENTIALS)
+      throw new Error("Bad environment config: Missing credentials");
+    const googleCredentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+    if (!process.env.GOOGLE_SPREADSHEET_ID)
+      throw new Error("Bad environment config: Missing spreadsheet ID");
+    const googleSpreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
 
-const handler: Handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(visits),
-  };
+    const auth = new google.auth.GoogleAuth({
+      credentials: googleCredentials,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    // Acquire an auth client, and bind it to all future calls
+    const authClient = await auth.getClient();
+    const sheets = google.sheets({ version: "v4", auth: authClient });
+
+    const spreadsheet = await sheets.spreadsheets.values.get({
+      spreadsheetId: googleSpreadsheetId,
+      range: "Visits!A1:E101",
+      valueRenderOption: "FORMULA",
+      dateTimeRenderOption: "SERIAL_NUMBER",
+    });
+
+    const values = spreadsheet.data.values;
+    if (!values) throw new Error("No data");
+
+    // Convert the column headers into prop names
+    const props = values[0].map((name: string, index: number) => {
+      if (!name) throw new Error(`No header value at ${COLUMNS[index]}1`);
+      return camelize(name);
+    });
+
+    const data = values.slice(1).reduce((result, row, i) => {
+      const id = i + 1;
+      result[id] = {
+        ...props.reduce((rowProps, name, pIndex) => {
+          rowProps[name] = row[pIndex];
+          return rowProps;
+        }, {} as { [key: string]: any }),
+        id,
+      };
+      return result;
+    }, {} as Record<number, { id: number; [key: string]: any }>);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+      headers: {
+        "Cache-Control": "s-maxage=1, stale-while-revalidate",
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
 };
 
-export { handler };
+const COLUMNS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const camelize = (str: string) =>
+  str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index: number) => {
+    if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+    return index === 0 ? match.toLowerCase() : match.toUpperCase();
+  });
